3dd4bd3ee9559d21e8eec7684cd86560b4aad1c8
Raymond Jacobson <me@raymondjacobson.com>
2016-04-01 12:33:52 -0400

    add checkpoint5.py boilerplate

---
+++ checkpoint5.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+client = MongoClient()
+
+
+def random_word_requester():
+    '''
+    This function should return a random word and its definition and also
+    log in the MongoDB database the timestamp that it was accessed.
+    '''
+    return
+
+
+if __name__ == '__main__':
+    print random_word_requester()
