edfdb0629c13c28bdfa7ea525d2c05a6677c8675
Ashwin <raghua@rpi.edu>
2016-04-12 20:04:53 -0400

    Checkpoint 3

--- checkpoint4.py
+++ checkpoint4.py
@@ -1,5 +1,9 @@
 from pymongo import MongoClient
-client = MongoClient()
+client = MongoClient('localhost',27017)
+db = client['csci2963']
+collection = db['definitions']
+posts = db.posts
 
 if __name__ == '__main__':
+    print posts.find_one()
     print "Modify me"
