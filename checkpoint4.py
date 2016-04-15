f24cb1944e0594cc43b18973147059de0dbd76a2
Ashwin <raghua@rpi.edu>
2016-04-15 10:39:14 -0400

    ckpt5

--- checkpoint4.py
+++ checkpoint4.py
@@ -1,4 +1,5 @@
 import pymongo
+import datetime
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 # Connection to Mongo DB
@@ -7,27 +8,27 @@ try:
     db = conn.csci2963
     definitions = db.definitions
 
-    print "Printing all entries in the database!"
-    for d in definitions.find():
-        print d
+    #print "Printing all entries in the database!"
+    #for d in definitions.find():
+    #    print d
 
 
-    print "Printing the first element in the database!"
-    print definitions.find_one()
+#    print "Printing the first element in the database!"
+#    print definitions.find_one()
 
 
-    print "fetching a specific item"
-    print definitions.find_one({"word": "Love Canal"})
+    #print "fetching a specific item"
+    #print definitions.find_one({"word": "Love Canal"})
 
 
-    print "fetching a specific item by _id"
-    print definitions.find_one({"_id": ObjectId("56fe9e22bad6b23cde07b8ce")})
+    #print "fetching a specific item by _id"
+    #print definitions.find_one({"_id": ObjectId("56fe9e22bad6b23cde07b8ce")})
 
 
-    print "Insert a new word into the database"
-    post = {"word" : "Hecht","definition" : "One who eat's many eggs"}
+    #print "Insert a new word into the database"
+    post = {"word" : "Hecht","definition" : "One who eat's many eggs","Timestamp": str(datetime.datetime.now())}
     post_id = definitions.insert_one(post).inserted_id
-    print post_id
+    print definitions.find_one({"_id": ObjectId(str(post_id))})
 
 
 except pymongo.errors.ConnectionFailure, e:
--- checkpoint5.py
+++ checkpoint5.py
@@ -1,8 +1,56 @@
+import pymongo
+import datetime
+import random
 from pymongo import MongoClient
-client = MongoClient()
+from bson.objectid import ObjectId
 
+client = MongoClient()
+conn=MongoClient()
+db = conn.csci2963
+definitions = db.definitions
 
 def random_word_requester():
+    #find a random record and print out the contents
+    collectionCount = db.definitions.count()
+    rand = random.randint(0,collectionCount)
+    randomRecord = db.definitions.find().limit(1).skip(rand).next()
+    randid = randomRecord['_id']
+    randword = randomRecord['word']
+    randdef = randomRecord['definition']
+    #
+
+    result = db.definitions.update_one(
+        {"word":randword},
+        {
+            "$set":{
+                "Timestamps":[]
+            }
+        }
+    )
+
+    result = db.definitions.update_one(
+        {"word":randword},
+        {
+            "$push":{
+                "Timestamps":str(datetime.datetime.now())
+            }
+        }
+    )
+
+
+    result = db.definitions.update_one(
+        {"word":randword},
+        {
+            "$push":{
+                "Timestamps":str(datetime.datetime.today())
+            }
+        }
+    )
+
+    updatedrecord = db.definitions.find_one({"word":randword})
+    print randomRecord['word'] + ": " + randomRecord['definition']
+    print updatedrecord['Timestamps']
+    #update the contents
     '''
     This function should return a random word and its definition and also
     log in the MongoDB database the timestamp that it was accessed.
