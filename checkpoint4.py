85a3a6e069b1601079b498e196706538643a360a
aleung013 <aleung013@gmail.com>
2016-04-29 01:31:41 -0400

    Checkpoint 4

--- checkpoint4.py
+++ checkpoint4.py
@@ -1,5 +1,29 @@
 from pymongo import MongoClient
-client = MongoClient()
+from bson.objectid import ObjectId
+client = MongoClient('localhost',25565)
 
 if __name__ == '__main__':
-    print "Modify me"
+    db = client['csci2963']
+    defs = db['definitions']
+
+    #Fetch all records
+    print 'Fetching all records'
+    for definition in defs.find():
+        print definition
+    
+    #Fetch one record
+    print 'Fetching first record'
+    print defs.find_one()
+    
+    #Fetch specific record
+    print 'Fetching Capitaland'
+    print defs.find_one({'word':'Capitaland'})
+    
+    #Fetch by object id
+    print 'Fetching by ObjectId'
+    print defs.find_one({'_id':ObjectId('56fe9e22bad6b23cde07b8ce')})
+
+    #Insert a new object
+    print 'Inserting dog'
+    defs.insert_one({'word':'dog','definition':"n. Man's best friend"})
+    print defs.find_one({'word':'dog'})
