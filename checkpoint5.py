dc2e296a449e3f950336d6224c94432c6f61fee5
aleung013 <aleung013@gmail.com>
2016-04-29 01:55:01 -0400

    Checkpoint 5

--- checkpoint5.py
+++ checkpoint5.py
@@ -1,13 +1,27 @@
 from pymongo import MongoClient
-client = MongoClient()
+from random import randint
+from bson.objectid import ObjectId
+import datetime
 
+client = MongoClient('localhost',25565)
+db = client['csci2963']
+defs = db['definitions']
 
 def random_word_requester():
     '''
     This function should return a random word and its definition and also
     log in the MongoDB database the timestamp that it was accessed.
     '''
-    return
+    words = [word for word in defs.find()]
+    rnum = randint(0,len(words))
+    rword = words[rnum]
+    try:
+        dates = rword['dates']
+    except:
+        dates = []
+    dates.append(str(datetime.datetime.utcnow()))
+    defs.find_one_and_update({'_id':ObjectId(rword['_id'])},{'$set':{'dates':dates}})
+    return defs.find_one({'_id':ObjectId(rword['_id'])})
 
 
 if __name__ == '__main__':
