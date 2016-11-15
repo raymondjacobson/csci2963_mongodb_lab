a6e9a09a92eb54f3f0ff3e350efd7b603cebce1c
Justin <jrcarlson8@gmail.com>
2016-11-15 14:38:40 -0500

    checkpoints 5 and 6

--- checkpoint4.py
+++ checkpoint4.py
@@ -2,4 +2,18 @@ from pymongo import MongoClient
 client = MongoClient()
 
 if __name__ == '__main__':
-    print "Modify me"
+	client = MongoClient('localhost', 27017)
+	db = client.csci2963
+	collection = db.definitions
+	# fetch all records
+	print 'all records:'
+	for i in collection.find():
+		print i
+	# fetch one record
+	print 'one record:', collection.find_one()
+	# fetch a specific record
+	print 'specific record:', collection.find({"word": "Foo"})
+	# fetch a record by object id
+	print 'record by object id:', collection.find({"_id": "56fe9e22bad6b23cde07b8ce"})
+	# insert a new record
+	collection.insert({"word": "definition"})
