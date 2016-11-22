352dcd6c9a70fb1395106f6f3fe01a745631524a
Sean Maltby <seanmaltby@gmail.com>
2016-11-22 13:23:57 -0500

    Lab 10

--- checkpoint4.py
+++ checkpoint4.py
@@ -1,5 +1,18 @@
 from pymongo import MongoClient
 client = MongoClient()
+from bson.objectid import ObjectId
 
 if __name__ == '__main__':
-    print "Modify me"
+	db = client.csci2963
+	defs = db.definitions
+
+	for definition in defs.find():
+		print definition
+
+	print defs.find_one()
+
+	print defs.find({"word": "Word"})
+
+	print defs.find_one({"_id": ObjectId('56fe9e22bad6b23cde07b8b7')})
+
+	defs.insert_one({"word": "NotAWord", "definition": "NotADefinition"})
