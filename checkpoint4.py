a0ac8a9290b3dcf4b4d46e455baf1be9f39d3c4c
grubeb2 <grubeb2@rpi.edu>
2016-11-22 13:24:35 -0500

    Update chepoint4.py

--- checkpoint4.py
+++ checkpoint4.py
@@ -1,5 +1,18 @@
 from pymongo import MongoClient
-client = MongoClient()
 
 if __name__ == '__main__':
-    print "Modify me"
+	from bson.objectid import ObjectId
+
+	client = MongoClientclient = MongoClient('localhost', 27017)
+	db = client.csci2963
+	db.authenticate("admin", "admin123", "admin")
+	collection = db.definitions
+
+	print collection.find_one()
+
+	for post in collection.find():
+		print post
+
+	print collection.find_one({"word": "hello"})
+	print collection.find_one({"_id": ObjectId('582ecdf52c178ef676e9e5ff')})
+	print collection.find_one({"_id": ObjectId(str(collection.insert({"word": "goodbye", "definition": "exclam. used to express good wishes when parting or at the end of a conversation."})))})
