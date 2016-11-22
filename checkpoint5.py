dd49128a1b81dcaba4813e3e9d2a59a90a4a7d6b
grubeb2 <grubeb2@rpi.edu>
2016-11-22 13:48:37 -0500

    Update checkpoint5.py

--- checkpoint5.py
+++ checkpoint5.py
@@ -3,12 +3,19 @@ client = MongoClient()
 
 
 def random_word_requester():
-    '''
-    This function should return a random word and its definition and also
-    log in the MongoDB database the timestamp that it was accessed.
-    '''
-    return
+	'''
+	This function should return a random word and its definition and also
+	log in the MongoDB database the timestamp that it was accessed.
+	'''
+	client = MongoClientclient = MongoClient('localhost', 27017)
+	db = client.csci2963
+	db.authenticate("admin", "admin123", "admin")
+	collection = db.definitions
+
+
+	collection.aggregate([{ $sample: { size: 1 } }])
+	return
 
 
 if __name__ == '__main__':
-    print random_word_requester()
+	print random_word_requester()
