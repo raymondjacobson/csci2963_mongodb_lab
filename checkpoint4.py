30dc5a08c61528cd0a1d3ca422b3124e72343154
Samuel Breese <chameco@tfwno.gf>
2017-04-11 12:51:43 -0400

    update

--- checkpoint4.py
+++ checkpoint4.py
@@ -1,5 +1,24 @@
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 client = MongoClient()
 
 if __name__ == '__main__':
-    print "Modify me"
+    db = client.csci2963
+    defs = db.definitions
+
+    print "\nFind all records:"
+    for x in defs.find({}):
+        print x
+
+    print "\nFind one record:"
+    print defs.find_one()
+
+    print "\nFind a specific record:"
+    print defs.find_one({"word": "Capitaland"})
+
+    print "\nFind a specific record:"
+    print defs.find_one({"_id": ObjectId('56fe9e22bad6b23cde07b949')})
+
+    print "\nInsert a record:"
+    defs.insert({"word": "foo", "definition": "bar"})
+    print defs.find_one({"word": "foo"})
